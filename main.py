3205ff092fef2b70d27191ac2752b12eebc839dd
Xer0-PT <joelflm@gmail.com>
2021-01-24 18:14:30 +0000

    Feito

--- main.py
+++ main.py
@@ -2,10 +2,7 @@
 
 from Parser import Parser
 
-
 parser = Parser()
-
-# Fazer um for para ler vário ficheiros FIXME
 
 with open("teste.logo", mode="r") as fh:
     contents = fh.read()
@@ -13,6 +10,3 @@ with open("teste.logo", mode="r") as fh:
 parser.Parse(contents)
 
 parser.turtle.file.save()
-
-# PROF fala sobre o enunciado na aula do dia 11-12-2020
-# Sugestão do PROF, posição inicial -> não começar no (0,0)
